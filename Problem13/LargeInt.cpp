befb99561499f0c026432aa5a0037715196bf9ba
Drew Burton <drewburton@users.noreply.github.com>
2018-04-08 12:59:32 -0400

    everything broken trying to refactor using classes

---
+++ Problem13/LargeInt.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "LargeInt.h"
+
+
+LargeInt::LargeInt()
+{
+
+}
+
+
+LargeInt::~LargeInt()
+{
+
+}
+
+void LargeInt::init(string number)
+{
+
+}
+
---
+++ Problem13/LargeInt.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+class LargeInt
+{
+public:
+    LargeInt();
+    ~LargeInt();
+
+    void init(string number);
+    //int digits[100];
+};
+
