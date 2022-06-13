875f62a5192325e9c697f8c0a437f9a1e88ea507
Drew Burton <andrew.burton@lcecorp.com>
2022-06-13 14:34:07 -0400

    finished problem 29

---
+++ C#/Problem29/Problem29/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Numerics;
+
+namespace Problem29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int limit = 100;
+            Console.WriteLine(GenerateTerms(limit).Count);
+        }
+
+        private static HashSet<BigInteger> GenerateTerms(int limit)
+        {
+            HashSet<BigInteger> terms = new();
+            for (int a = 2; a <= limit; a++)
+            {
+                for (int b = 2; b <= limit; b++)
+                {
+                    terms.Add(BigInteger.Pow(a, b));
+                }
+            }
+            return terms;
+        }
+    }
+}
