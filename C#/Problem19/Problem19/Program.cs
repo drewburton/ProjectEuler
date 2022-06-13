e92aed059c592cb75395bb4ad2ab64de6f2072c7
Drew Burton <andrew.burton@lcecorp.com>
2022-06-13 15:33:03 -0400

    finished problem 19

---
+++ C#/Problem19/Problem19/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Problem19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime start = new(1901, 1, 1);
+            DateTime end = new(2000, 12, 31);
+            DateTime now = start;
+            int sundays = 0;
+            while (DateTime.Compare(now, end) < 0)
+            {
+                if (now.DayOfWeek == DayOfWeek.Sunday) sundays++;
+                now = now.AddMonths(1);
+            }
+            Console.WriteLine(sundays);
+        }
+    }
+}
