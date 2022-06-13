c330ace53e829fe1fe82bf220b4a81379ed109f4
Drew Burton <andrew.burton@lcecorp.com>
2022-06-13 14:51:08 -0400

    finished problem 30, digit fifth powers

---
+++ C#/Problem30/Problem30/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Problem30
+{
+    class Program
+    {
+        private const int _limit = 354294; // 6 * (9^5)
+        private static readonly List<int> digitFifthPowers = new();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SumDigitFifthPowers());
+        }
+
+        private static long SumDigitFifthPowers()
+        {
+            for (int i = 2; i < _limit; i++)
+            {
+                if (CanBeWrittenAsSumOfFifthPowersOfDigits(i))
+                    digitFifthPowers.Add(i);
+            }
+            return digitFifthPowers.Sum();
+        }
+
+        private static bool CanBeWrittenAsSumOfFifthPowersOfDigits(int num)
+        {
+            int sum = 0;
+            int n = num;
+            while (n > 0)
+            {
+                sum += (int) Math.Pow(n % 10, 5);
+                n /= 10;
+            }
+            return sum == num;
+        }
+    }
+}
