9b3ffe3ae328be2a33ad1f54b34c76fb42b4b5f4
Drew Burton <Andrew.Burton@LCEcorp.com>
2022-07-27 10:32:51 -0400

    finished problem 32

---
+++ C#/Problem32/Problem32/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Problem32
+{
+    class Program
+    {
+        private const int PANDIGIAL_LENGTH = 9;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetPandigitalProducts().Sum());
+        }
+
+        private static HashSet<long> GetPandigitalProducts()
+        {
+            HashSet<long> products = new();
+            for (long a = 1; a < 9999; a++)
+            {
+                for (long b = a + 1; b < 9999; b++)
+                {
+                    long product = a * b;
+                    if (IsPandigital(a, b, product)) products.Add(product);
+                }
+            }
+            return products;
+        }
+
+        private static bool IsPandigital(long multiplicand, long multiplier, long product)
+        {
+            string combined = $"{multiplicand}{multiplier}{product}";
+            return combined.Length == PANDIGIAL_LENGTH && 
+                combined.Distinct().AsEnumerable().Count() == PANDIGIAL_LENGTH &&
+                !combined.Contains('0');
+        }
+    }
+}
